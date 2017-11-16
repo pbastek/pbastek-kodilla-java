31d2c23c4fa6584282b5b2535515b164cea6ca4f
pbastek <piotrbastek@poczta.fm>
2017-11-16 22:54:25 +0100

    Collection Testing

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/user/SimpleUser.java
@@ -0,0 +1,10 @@
+package com.kodilla.testing.user;
+public class SimpleUser {
+    String username;
+    public SimpleUser(String username){
+        this.username = username;
+    }
+    public String getUsername() {
+        return username;
+    }
+}
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -0,0 +1,69 @@
+package com.kodilla.testing.collection;
+
+import org.junit.*;
+import java.util.*;
+
+public class CollectionTestSuite {
+
+    @Before
+    public void before(){
+
+        System.out.println("Test Case: begin");
+    }
+    @After
+    public void after(){
+
+        System.out.println("Test Case: end");
+    }
+
+    @Test
+    public void testOddNumbersExterminatorEmptyList(){
+        //Given
+        ArrayList<Integer> emptyList = new ArrayList<Integer>();
+
+        //When
+        System.out.println("Empty list test.");
+        OddNumbersExterminator exterminator = new OddNumbersExterminator();
+        ArrayList<Integer> result = exterminator.exterminate(emptyList);
+
+        //Then
+        Assert.assertEquals(emptyList,result);
+    }
+    @Test
+    public void testOddNumbersExterminatorNullList(){
+        //Given
+        ArrayList<Integer> nullList = null;
+        ArrayList<Integer> emptyList = new ArrayList<Integer>();
+
+        //When
+        System.out.println("Null list testing");
+        OddNumbersExterminator exterminator = new OddNumbersExterminator();
+        ArrayList<Integer> result = exterminator.exterminate(nullList);
+
+        //Then
+        Assert.assertEquals(emptyList,result);
+
+        Assert.assertTrue("Kolekcja jest pusta",result!=null);
+        Assert.assertTrue("Kolekcja nie jest pusta",result.isEmpty());
+    }
+    @Test
+    public void testOddNumbersExterminatorNormalList (){
+        //Given
+        ArrayList<Integer> checkList = new ArrayList<Integer>();
+        ArrayList<Integer> evenList = new ArrayList<Integer>();
+        for(int i=0;i<100;i++){
+            checkList.add(i);
+            if(i%2==0){
+                evenList.add(i);
+            }
+        }
+
+        //When
+        System.out.println("Regular list testing");
+        OddNumbersExterminator exterminator = new OddNumbersExterminator();
+        ArrayList<Integer> result = exterminator.exterminate(checkList);
+
+        //Then
+        Assert.assertEquals(evenList,result);
+    }
+}
