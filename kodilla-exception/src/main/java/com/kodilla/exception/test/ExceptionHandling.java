2a566ced9b6fae6d789b31c9fc4977e2157e6358
pbastek <piotrbastek@poczta.fm>
2017-11-22 19:33:45 +0100

    Exception Handling

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -2,5 +2,16 @@ package com.kodilla.exception.test;
 
 public class ExceptionHandling {
 
+    public static void main(String[] args) {
 
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        try {
+            secondChallenge.probablyIWillThrowException(1.5, 1.5);
+        } catch (Exception e) {
+            System.out.println("It is an exception");
+        } finally {
+            System.out.println("The method probablyIWillThrowException has just been called out");
+        }
+    }
 }
